48f3b9a680f347302ee8078def7ce8478da5a6bf
csnova <chandlersnova@gmail.com>
2023-09-12 14:58:57 -0700

    Fixed Buttons Added the logic to clear page and run the module for the button that was pressed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,39 @@ import aboutPage from "./aboutPage";
 import contactPage from "./contactPage";
 
 homePage();
-// menuPage();
-// aboutPage();
-// contactPage();
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    changePage(button.className);
+  });
+});
+
+function removeElementsByClass(className) {
+  const elements = document.getElementsByClassName(className);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+function changePage(className) {
+  removeElementsByClass("contentBox");
+  if (className === "homePage") {
+    homePage();
+  }
+  if (className === "menu") {
+    menuPage();
+  }
+  if (className === "about") {
+    aboutPage();
+  }
+  if (className === "contact") {
+    contactPage();
+  }
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      changePage(button.className);
+    });
+  });
+}
